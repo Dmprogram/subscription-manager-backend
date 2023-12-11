3b073ea1e160692775e33a4cdd994ffb1db1d633
Corgi <dmprogramhh@mail.ru>
2023-12-11 20:33:54 +0300

    change-sort-and-return-subcription-fields

--- src/subscription/subscription.object.ts
+++ src/subscription/subscription.object.ts
@@ -1,9 +1,6 @@
 import { Prisma } from '@prisma/client'
 
 export const subcriptionObject: Prisma.SubscriptionSelect = {
-  user: {
-    select: { id: true },
-  },
   id: true,
   name: true,
   currency: true,
