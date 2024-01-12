88268cdde584febdeadec5809823e1413d37ed51
Corgi <dmprogramhh@mail.ru>
2024-01-12 22:03:00 +0300

    delete-added-cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.setGlobalPrefix('api')
-  app.enableCors({ origin: true, methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', credentials: true })
+  app.enableCors()
   await app.listen(4200)
 }
 bootstrap()
